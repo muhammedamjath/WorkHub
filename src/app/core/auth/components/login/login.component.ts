30959ccf0b8c1f3fe89ee13a5705903683f76ef5
Amjath <muhammedamjath0@gmail.com>
2025-03-30 14:20:19 +0530

    login page created

---
+++ src/app/core/auth/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  error: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+    } else {
+    }
+  }
+}
